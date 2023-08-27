1292a25c7189e3a5f44f6d3613c27b2c43e7433d
laabouli <mohammedlaabouli@gmail.com>
2023-08-28 00:35:32 +0100

    create navbar and hero section

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,67 @@
+import React, { useState } from "react";
+import "./header.css";
+
+const Header = () => {
+  // toggle menu
+  const [Toggle, showMenu] = useState(false);
+  return (
+    <div className="header">
+      <div className="nav container">
+        <a href="index.html" className="nav__logo">
+          Mohamed
+        </a>
+        <div className={Toggle ? "nav__menu show-menu" : "nav__menu"}>
+          <ul className="nav__list grid">
+            <li className="nav__item">
+              <a href="#Home" className="nav__link active-link">
+                <i className="uil uil-estate nav__icon"></i>Home
+              </a>
+            </li>
+
+            <li className="nav__item">
+              <a href="#About" className="nav__link">
+                <i className="uil uil-user nav__icon"></i>About
+              </a>
+            </li>
+
+            <li className="nav__item">
+              <a href="#Skills" className="nav__link">
+                <i className="uil uil-file-alt nav__icon"></i>Skills
+              </a>
+            </li>
+
+            <li className="nav__item">
+              <a href="#services" className="nav__link">
+                <i className="uil uil-briefcase-alt nav__icon"></i>
+                services
+              </a>
+            </li>
+
+            <li className="nav__item">
+              <a href="#portfolio" className="nav__link">
+                <i className="uil uil-scenery nav__icon"></i>
+                portfolio
+              </a>
+            </li>
+
+            <li className="nav__item">
+              <a href="#contact" className="nav__link">
+                <i className="uil uil-scenery nav__icon"></i>
+                Contact
+              </a>
+            </li>
+          </ul>
+          <i
+            class="uil uil-times nav__close"
+            onClick={() => showMenu(!Toggle)}
+          ></i>
+        </div>
+        <div className="nav__toggle" onClick={() => showMenu(!Toggle)}>
+          <i class="uil uil-apps"></i>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/components/home/Social.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+const Social = () => {
+  return (
+    <div className="home__social">
+      <a
+        href="mailto:mohammedlaabouli@gmail.com"
+        className="home__social-icon"
+        target="_blank"
+      >
+        <i class="uil uil-envelope-alt"></i>
+      </a>
+
+      <a
+        href="https://www.linkedin.com/in/mohamed-laabouli-66b57b1bb/"
+        className="home__social-icon"
+        target="_blank"
+      >
+        <i class="uil uil-linkedin-alt"></i>
+      </a>
+
+      <a
+        href="https://github.com/laabouli"
+        className="home__social-icon"
+        target="_blank"
+      >
+        <i class="uil uil-github-alt"></i>
+      </a>
+    </div>
+  );
+};
+
+export default Social;
